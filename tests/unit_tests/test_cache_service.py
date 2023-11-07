3a00244463c39e8bd1dd65a6b77762d39d1f4261
alexey_t <alexterleev682004@gmail.com>
2023-11-07 21:52:50 +0300

    add redis

--- tests/unit_tests/test_cache_service.py
+++ tests/unit_tests/test_cache_service.py
@@ -7,14 +7,14 @@ class TestCacheService:
     @pytest.mark.asyncio
     async def test_tocken_not_in_balcklist(self):
         service = cache_service()
-        token_in_blacklist = await service.token_in_blackist("test_tocken")
+        token_in_blacklist = await service.token_in_blackist("test_token")
         assert not token_in_blacklist
 
     @pytest.mark.asyncio
     async def test_balcklist_token(self):
         service = cache_service()
-        await service.blacklist_token("test_tocken")
-        token_in_blacklist = await service.token_in_blackist("test_tocken")
+        await service.blacklist_token("test_token")
+        token_in_blacklist = await service.token_in_blackist("test_token")
         assert token_in_blacklist
 
-        await service.cache_repo.srem("token_blacklist", "test_tocken")
+        await service.cache_repo.srem("token_blacklist", "test_token")
